91c9c42fa25a50f54a5c024bf96c46ade71a820e
EY-X <e.yosefx128@gmail.com>
2019-09-03 01:23:12 -0400

    working

--- src/FilterItem.js
+++ src/FilterItem.js
@@ -1,22 +1,58 @@
 import React from "react";
 
-const FilterItem = ({ filter, id, handleFilterSelected }) => {
-  const { name, value } = filter;
+class FilterItem extends React.Component {
+  componentDidMount = () => {
+    console.log(this.props.filters);
+    this.setState({
+      filter: this.props.filter,
+      id: this.props.id,
+      handleFilterSelected: this.props.handleFilterClick
+    });
+  };
 
-  return (
-    <li>
-      <input
-        type="radio"
-        name="category"
-        value={value}
-        id={id}
-        defaultChecked
-      />
-      <label onClick={() => handleFilterSelected(value)} htmlFor="filter0">
-        {name}
-      </label>
-    </li>
-  );
-};
+  render() {
+    return (
+      <li>
+        <input
+          type="radio"
+          name="category"
+          value={this.props.filter.value}
+          id={this.props.id}
+          defaultChecked
+        />
+        <label
+          onClick={() =>
+            this.state.handleFilterSelected(this.props.filter.value)
+          }
+          htmlFor="filter0"
+        >
+          {this.props.filter.name}
+        </label>
+      </li>
+    );
+  }
+}
 
 export default FilterItem;
+
+// const FilterItem = ({ filter, id, handleFilterSelected }) => {
+//   const { name, value } = filter;
+
+//   return (
+//     <li>
+//       <input
+//         type="radio"
+//         name="category"
+//         value={value}
+//         id={id}
+//         defaultChecked
+//       />
+
+//       <label onClick={() => handleFilterSelected(value)} htmlFor="filter0">
+//         {name}
+//       </label>
+//     </li>
+//   );
+// };
+
+// export default FilterItem;
--- src/Filters.js
+++ src/Filters.js
@@ -1,37 +1,89 @@
 import React from "react";
 import FilterItem from "./FilterItem";
 
-const Filters = ({ filters, onFilterClick }) => {
-  return (
-    <section id="filterCategories">
-            
-      <ul className="filters">
-                
-        {filters.map((filter, i) => (
-          <FilterItem
-            key={i}
-            id={i}
-            filter={filter}
-            handleFilterSelected={onFilterClick}
+class Filters extends React.Component {
+  handleFilterClick = () => {};
+  constructor(props) {
+    super(props);
+
+    this.handleFilterClick = this.handleFilterClick.bind(this);
+  }
+
+  componentDidMount = () => {
+    console.log(this.props.filters);
+    this.setState({
+      filters: this.props.filters
+    });
+  };
+
+  render() {
+    return (
+      <section id="filterCategories">
+              
+        <ul className="filters">
+          {this.props.filters.map((filter, i) => (
+            <FilterItem
+              key={i}
+              id={i}
+              filter={filter}
+              // handleFilterSelected={handleFilterClick}
+            />
+          ))}
+        </ul>
+              
+        <form id="newCat" className="cat-new">
+                  
+          <input
+            type="text"
+            name="item"
+            id="itemName"
+            className="form-component inpt"
+            placeholder="New Category"
           />
-        ))}
-              
-      </ul>
-            
-      <form id="newCat" className="cat-new">
-                
-        <input
-          type="text"
-          name="item"
-          id="itemName"
-          className="form-component inpt"
-          placeholder="New Category"
-        />
-              
-      </form>
-          
-    </section>
-  );
-};
+                
+        </form>
+            
+      </section>
+    );
+  }
+}
 
 export default Filters;
+
+// const Filters = ({ filters, onFilterClick }) => {
+//   return (
+//     <section id="filterCategories">
+//
+//       <ul className="filters">
+//
+//         {filters.map((filter, i) => {
+//           //console.log(filter);
+//           return (
+//             <FilterItem
+//               key={i}
+//               id={i}
+//               filter={filter}
+//               handleFilterSelected={onFilterClick}
+//             />
+//           );
+//         })}
+//
+//       </ul>
+//
+//       <form id="newCat" className="cat-new">
+//
+//         <input
+//           type="text"
+//           name="item"
+//           id="itemName"
+//           className="form-component inpt"
+//           placeholder="New Category"
+//         />
+//
+//       </form>
+//
+//     </section>
+//   );
+// };
+
+// export default Filters;
